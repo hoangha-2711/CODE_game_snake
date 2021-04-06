093d0b974941cdad4f0a772d203a05aab7d441d1
hoangha-2711 <80314055+hoangha-2711@users.noreply.github.com>
2021-04-06 21:53:55 +0700

    Create image.cpp

---
+++ image.cpp
@@ -0,0 +1,61 @@
+#include "image.h"
+#include <iostream>
+
+image::image()
+{
+    texture_snake = NULL;
+    texture_fruits = NULL;
+    return;
+}
+
+image::~image()
+{
+    texture_snake = NULL;
+    texture_fruits = NULL;
+    return;
+}
+
+void image::init(SDL_Renderer* _renderer)
+{
+    renderer = _renderer;
+    return;
+}
+
+void image::Load()
+{
+    //lazyfoo
+    return;
+}
+
+void image::draw_snake(SDL_Rect dest)
+{
+    SDL_RenderCopy(renderer, texture_snake, NULL, &dest);
+    return;
+}
+
+void image::draw_fruits(SDL_Rect dest)
+{
+    SDL_RenderCopy(renderer, texture_fruits, NULL, &dest);
+    return;
+}
+
+SDL_Texture* loadFromFIle(SDL_Renderer* renderer, std::string path)
+{
+    SDL_Texture* tmpTexture = NULL;
+    SDL_Surface* tmpSurface = IMG_Load(path.c_str());
+    if (tmpSurface == NULL)
+    {
+        //Error:
+        return tmpTexture;
+    }
+
+    tmpTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+    if (tmpTexture == NULL)
+    {
+        //Error:
+        return tmpTexture;
+    }
+
+    //cout << done!
+    return tmpTexture;
+}
