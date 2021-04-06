3185efd621f718377d67e4c6d12000b23ab1d5ed
hoangha-2711 <80314055+hoangha-2711@users.noreply.github.com>
2021-04-06 21:53:18 +0700

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,26 @@
+#include "game.h"
+#include <iostream>
+
+game::game()
+{
+
+}
+
+game::~game()
+{
+
+}
+
+void game::handleKeyPress(int key_code)
+{
+
+}
+void game::loop()
+{
+
+}
+
+void game::render()
+{
+
+}
