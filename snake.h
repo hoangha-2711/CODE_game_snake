fdb3668457bc4dda44e195182eeb482797a416af
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-19 12:23:01 +0700

    Update snake.h

--- snake.h
+++ snake.h
@@ -4,5 +4,21 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+struct snake
+{
+    //constructor
+    snake();
+    //destructor
+    ~snake();
+    point.body[500];
+    int fruits_count;
+    void init(int x, int y, SDL_Renderer* _renderer);
+    void loop();
+    void render();
+    void dead();
+    SDL_Renderer* renderer;
+    int cur_length;
+    SDL_Texture* texture;
+};
 
 #endif // SNAKE_H_
