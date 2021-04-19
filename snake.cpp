91a664dca58c4db913c098887e015f7761e1734c
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-19 12:22:31 +0700

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -1,3 +1,37 @@
 #include "snake.h"
 #include <iostream>
 #include <string>
+
+snake::snake()
+{
+
+}
+
+snake::~snake()
+{
+
+}
+
+snake::init(int x, int y,SDl_Renderer* _renderer)
+{
+    cur_length = 1;
+    body[0] = point(x,y);
+    renderer = _renderer;
+    fruits_count = 0;
+}
+
+snake::loop()
+{
+
+
+}
+
+snake::render()
+{
+    texture =
+}
+
+snake::dead()
+{
+
+}
