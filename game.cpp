9836dc63c841f5c1e0e648d33b96586663e435ea
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-26 22:45:12 +0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -11,16 +11,59 @@ game::~game()
 
 }
 
+void game::init(SDL_Renderer* renderer)
+{
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(renderer);
+
+    gameBackground.init(renderer);
+    gameBackground.load();
+    snakeImage.init(renderer);
+    snakeImage.load();
+    fruitImage.init(renderer);
+    fruitImage.load();
+}
+
 void game::handleKeyPress(int key_code)
 {
-
+    switch(key_code)
+    {
+        case SDLK_UP:
+            player.setDirection(Up);
+            break;
+        case SDLK_DOWN:
+            player.setDirection(Down);
+            break;
+        case SDLK_LEFT:
+            player.setDirection(Left);
+            break;
+        case SDLK_RIGHT:
+            player.setDirection(Right);
+            break;
+    }
+    player.snakeMove();
 }
-void game::loop()
+void game::loop(bool& running)
 {
-
+    SDL_Delay(GAME_DELAY);
+    player.snakeMove();
+    score.spawnFruit();
+    SDL_Rect playerRect = {player.getPosX(), player.getPosY(), SNAKE_CELL, SNAKE_CELL};
+    SDL_Rect scoreRect = {score.getPosX(), score.getPosY(), FRUIT_CELL, FRUIT_CELL};
+    if(Collision(playerRect, scoreRect))
+    {
+        score.decreaseFruit();
+        player.grownUp();
+    }
+    if(player.dead()) running = false;
 }
 
-void game::render()
+void game::render(SDL_Renderer* renderer)
 {
+    gameBackground.display();
 
+    snakeImage.draw_snake(player.getCell());
+    if(score.countFruit() > 0) fruitImage.draw_fruit(score.getPosX(), score.getPosY());
+
+    SDL_RenderPresent(renderer);
 }
