c81ed796c374e9abbbdc7fb7a6136652c648345a
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-26 22:47:07 +0700

    Update fruits.h

--- fruits.h
+++ fruits.h
@@ -1,8 +1,26 @@
 #ifndef FRUITS_H_
 #define FRUITS_H_
+#include "BaseInfo.h"
 #include <bits/stdc++.h>
 #include <SDL.h>
 #include <SDL_image.h>
 
+const int MAX_FRUIT = 1;
+
+class fruits
+{
+public:
+    //constructor
+    fruits();
+    //destructor
+    ~fruits();
+    void spawnFruit();
+    int countFruit();
+    void decreaseFruit();
+    int getPosX();
+    int getPosY();
+private:
+    int x, y, numFruit;
+};
 
 #endif // FRUITS_H_
