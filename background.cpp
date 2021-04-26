019e19e19b6c89110507cb558f05b4572aae100d
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-26 22:47:57 +0700

    Update background.cpp

--- background.cpp
+++ background.cpp
@@ -1,3 +1,52 @@
 #include "background.h"
 #include <iostream>
 #include <string>
+
+background::background()
+{
+    background_game = NULL;
+    wall = NULL;
+}
+background::~background()
+{
+    background_game = NULL;
+    wall = NULL;
+}
+void background::init(SDL_Renderer* _renderer)
+{
+    renderer = _renderer;
+}
+void background::render(SDL_Renderer* des, SDL_Texture* texture, const int& rectX, const int& rectY, const int& reW, const int& reH, SDL_Rect* clip)
+{
+    SDL_Rect renderQuad = {rectX, rectY, reW, reH};
+    SDL_RenderCopy(des, texture , clip, &renderQuad);
+}
+void background::load()
+{
+    background_game = loadTexture(renderer, "background_brown.png");
+    wall = loadTexture(renderer, "block_square.png");
+}
+void background::display()
+{
+    render(renderer, background_game, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // Load wall up and down
+    for(int x = 0; x <= SCREEN_WIDTH; x += WALL_CELL)
+    {
+        render(renderer, wall, x, 0, WALL_CELL, WALL_CELL);
+        render(renderer, wall, x, SCREEN_HEIGHT - WALL_CELL, WALL_CELL, WALL_CELL);
+    }
+    // Load wall left and right
+    for(int y = 0; y <= SCREEN_HEIGHT; y += WALL_CELL)
+    {
+        render(renderer, wall, 0, y, WALL_CELL, WALL_CELL);
+        render(renderer, wall, SCREEN_WIDTH - WALL_CELL, y, WALL_CELL, WALL_CELL);
+    }
+}
+void background::free()
+{
+    SDL_DestroyTexture(background_game);
+    background_game = NULL;
+    SDL_DestroyTexture(wall);
+    wall = NULL;
+}
