73a0a6bebc79ab8def528b0f3375425a3a628641
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-26 22:44:48 +0700

    Update game.h

--- game.h
+++ game.h
@@ -1,17 +1,29 @@
 #ifndef GAME_H_
 #define GAME_H_
 
+#include "BaseInfo.h"
+#include "background.h"
+#include "image.h"
+#include "snake.h"
+#include "fruits.h"
+
 struct game
 {
     // Constructor
     game();
     //Destructor
     ~game();
+    void init(SDL_Renderer* renderer);
     void handleKeyPress(int key_code);
-    void loop();
-    void render();
+    void loop(bool& running);
+    void render(SDL_Renderer* renderer);
 
+    background gameBackground;
+    image snakeImage;
+    image fruitImage;
 
+    snake player;
+    fruits score;
 };
 
 
