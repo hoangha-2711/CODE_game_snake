024331cf31da91ef32b698386d0f74b31194d111
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-26 22:45:35 +0700

    Update image.h

--- image.h
+++ image.h
@@ -1,9 +1,11 @@
 #ifndef IMAGE_H_
 #define IMAGE_H_
-
+#include "BaseInfo.h"
+#include "texture_manage.h"
+#include <string>
 #include <SDL.h>
 #include <SDL_image.h>
-
+#include  <SDL_ttf.h>
 struct image
 {
     //Constructor
@@ -12,19 +14,17 @@ struct image
     ~image();
 
     void init(SDL_Renderer* _renderer);
-    void Load();
-    void draw_snake(SDL_Rect dest);
-    void draw_fruits(SDL_Rect dest);
+    void render(SDL_Renderer* des, SDL_Texture* texture, const int& rectX = 0, const int& rectY = 0, const int& reW = 0, const int& reH = 0, SDL_Rect* clip = NULL);
+    void load();
+    void draw_snake(const vector<SDL_Rect>& snakeCell);
+    void draw_fruit(const int& posX, const int& posY);
     void free();
 
     SDL_Renderer* renderer;
     SDL_Texture* texture_snake;
-    SDL_Texture* texture_fruits;
+    SDL_Texture* texture_fruit;
     //SDL_Rect snake_sprite;
 
 };
 
-SDL_Texture* loadFromFIle(std::string path);
-
 #endif // IMAGE_H_
-
