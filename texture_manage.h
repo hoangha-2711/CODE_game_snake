19c05e41ed06dcb47e15a2eed5cb6756f9eea629
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-26 22:48:21 +0700

    Update texture_manage.h

--- texture_manage.h
+++ texture_manage.h
@@ -1,12 +1,11 @@
 #ifndef TEXTURE_MANAGE_H_
 #define TEXTURE_MANAGE_H_
-
+#include <bits/stdc++.h>
 #include <SDL.h>
 
+using namespace std;
 
-SDL_Texture* loadTexture(std::string path);
-
-
+SDL_Texture* loadTexture(SDL_Renderer* renderer, string path);
 
 #endif // TEXTURE_MANAGE_H_
 
