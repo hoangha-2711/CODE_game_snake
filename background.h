d27011ade7dd05444b6a198b9abae2cbacad6414
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-26 22:47:39 +0700

    Update background.h

--- background.h
+++ background.h
@@ -1,8 +1,29 @@
 #ifndef BACKGROUND_H_
 #define BACKGROUND_H_
+#include "BaseInfo.h"
+#include "texture_manage.h"
 #include <bits/stdc++.h>
 #include <SDL.h>
 #include <SDL_image.h>
 
+const int WALL_CELL = 50;
+
+struct background
+{
+    //Constructor
+    background();
+    //Destructor
+    ~background();
+
+    void init(SDL_Renderer* _renderer);
+    void render(SDL_Renderer* des, SDL_Texture* texture, const int& rectX = 0, const int& rectY = 0, const int& reW = 0, const int& reH = 0, SDL_Rect* clip = NULL);
+    void load();
+    void display();
+    void free();
+
+    SDL_Renderer *renderer;
+    SDL_Texture *background_game;
+    SDL_Texture *wall;
+};
 
 #endif // BACKGROUND_H_
