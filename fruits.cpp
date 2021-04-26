7e15c18094176ea4f936b4601f6d725ea86c8488
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-26 22:47:23 +0700

    Update fruits.cpp

--- fruits.cpp
+++ fruits.cpp
@@ -1,3 +1,40 @@
 #include "fruits.h"
-#include <iostream>
-#include <string>
+
+fruits::fruits()
+{
+    x = 0, y = 0, numFruit = 0;
+}
+fruits::~fruits()
+{
+
+}
+void fruits::spawnFruit()
+{
+    if(countFruit() < MAX_FRUIT)
+    {
+        x = rand() % SCREEN_WIDTH;
+        y = rand() % SCREEN_HEIGHT;
+        if(x < 2 * LIMIT_LEFT) x = 2 * LIMIT_LEFT;
+        if(x > SCREEN_WIDTH - 2 * LIMIT_RIGHT - FRUIT_CELL) x = SCREEN_WIDTH - 2 * LIMIT_RIGHT - FRUIT_CELL;
+        if(y < 2 * LIMIT_UP) y = 2 * LIMIT_UP;
+        if(y > SCREEN_HEIGHT - 2 * LIMIT_DOWN - FRUIT_CELL) y = SCREEN_HEIGHT - 2 * LIMIT_DOWN - FRUIT_CELL;
+        numFruit++;
+    }
+}
+int fruits::countFruit()
+{
+    return numFruit;
+}
+void fruits::decreaseFruit()
+{
+    numFruit--;
+}
+int fruits::getPosX()
+{
+    return x;
+}
+int fruits::getPosY()
+{
+    return y;
+}
+
