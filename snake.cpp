689350de31e2be90fa3fe573c8746b4a8fea4816
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-28 13:09:13 +0700

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -1,10 +1,19 @@
 #include "snake.h"
+#include "sources.h"
 #include <iostream>
 #include <string>
 
 snake::snake()
 {
-
+    snakePart.x = SCREEN_WIDTH / 4;
+    snakePart.y = SCREEN_HEIGHT / 2; 
+    snakePart.h = SNAKE_SIZE; 
+    snakePart.w = SNAKE_SIZE;
+    direct = None; 
+    preDirect = None;
+    snakeCell.push_back(snakePart);
+    isDead = false;
+    grow = false;
 }
 
 snake::~snake()
@@ -12,26 +21,100 @@ snake::~snake()
 
 }
 
-snake::init(int x, int y,SDl_Renderer* _renderer)
+void snake::setDirection(int newDirect)
 {
-    cur_length = 1;
-    body[0] = point(x,y);
-    renderer = _renderer;
-    fruits_count = 0;
+    if(direct == None)
+    {
+        preDirect = direct;
+        direct = newDirect;
+    }
+    else if((direct == Up && newDirect != Down) || (direct == Down && newDirect != Up)) 
+    {
+        preDirect = direct;
+        direct = newDirect;
+    }
+    else if((direct == Left && newDirect != Right) || (direct == Right && newDirect != Left))
+    {
+        preDirect = direct;
+        direct = newDirect;
+    }
 }
 
-snake::loop()
+void snake::eraseTail()
 {
-
-
+    snakeCell.erase(snakeCell.begin());
 }
 
-snake::render()
+void snake::snakeMove()
 {
-    texture =
+    switch(direct)
+    {
+    case Up:
+        if(preDirect != Down)
+        {
+            snakePart.y -= SNAKE_SPEED; 
+            snakeCell.push_back(snakePart);
+            if(!grow) eraseTail();
+            else grow = false;
+            if(snakePart.y < LIMIT_UP) isDead = true;//snakePart.y += SNAKE_SPEED;
+        }
+        break;
+    case Down:
+        if(preDirect != Up)
+        {
+            snakePart.y += SNAKE_SPEED;
+            snakeCell.push_back(snakePart);
+            if(!grow) eraseTail();
+            else grow = false;
+            if(snakePart.y > SCREEN_HEIGHT - LIMIT_DOWN - SNAKE_CELL) isDead = true;//snakePart.y -= SNAKE_SPEED;
+        }
+        break;
+    case Left:
+        if(preDirect != Right)
+        {
+            snakePart.x -= SNAKE_SPEED;
+            snakeCell.push_back(snakePart);
+            if(!grow) eraseTail();
+            else grow = false;
+            if(snakePart.x < LIMIT_LEFT) isDead = true;//snakePart.x += SNAKE_SPEED;
+        }
+        break;
+    case Right:
+        if(preDirect != Left)
+        {
+            snakePart.x += SNAKE_SPEED;
+            snakeCell.push_back(snakePart);
+            if(!grow) eraseTail();
+            else grow = false;
+            if(snakePart.x > SCREEN_WIDTH - LIMIT_RIGHT - SNAKE_CELL) isDead = true;//snakePart.x -= SNAKE_SPEED;
+        }
+        break;
+    case None:
+        break;
+    }
 }
 
-snake::dead()
+void snake::grownUp()
 {
+    grow = true;
+}
 
+bool snake::dead()
+{
+    return isDead;
+}
+
+vector<SDL_Rect> snake::getCell()
+{
+    return snakeCell;
+}
+
+int snake::getPosX()
+{
+    return snakePart.x;
+}
+
+int snake::getPosY()
+{
+    return snakePart.y;
 }
