281f6c0baa498fb88f20e3a95cd8335f32959cc6
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-29 20:54:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,45 @@
 #include "sources.h"
+#include "menu.h"
 
 const std::string WINDOW_TITLE = "SDL Game";
 
 int main(int argc, char* argv[])
 {
-    srand(time(NULL));
-    sources* Gamesources;
-    Gamesources = new sources();
+    srand(time(NULL));// khởi tạo hạt giống ngẫu nhiên để rand chuẩn
+    sources* Gamesources;// game chính
+    Gamesources = new sources();// cấp phát động
 
     Gamesources -> init(SCREEN_WIDTH
                   ,SCREEN_HEIGHT
-                  ,WINDOW_TITLE);
+                  ,WINDOW_TITLE);// khởi tạo game
 
-    while (Gamesources->isRunning())
+    menu menuSource;
+    menuSource.init(Gamesources->renderer);
+    while(menuSource.isRunning())
     {
-        Gamesources->handleEvent();
+        if(!menuSource.isLoaded())
+        {
+            menuSource.setLoad(true);
+            menuSource.displayImage();
+            menuSource.writeText("<< Fast space snake >>", 120, 250);
+            menuSource.writeText("*Press Enter to play", 150, 500);
+            menuSource.writeText("*Q to Quit", 350, 650);
+        }
+        menuSource.handleKeyPresses();
+        if(menuSource.playGame())
+        {
+            while(Gamesources->isRunning())
+            {
+                Gamesources->handleEvent();
+                
+                Gamesources->loop();
 
-        Gamesources->loop();
-
-        Gamesources->render();
+                Gamesources->render();
+            }
+            menuSource.reset();
+            Gamesources->reset();
+        }
+        else if(menuSource.quitGame()) break;
     }
 
     delete Gamesources;
