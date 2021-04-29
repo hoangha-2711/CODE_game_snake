3ac2124a3b0098a2ee76d315f46813ef36cd35ab
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-29 20:56:03 +0700

    Update game.h

--- game.h
+++ game.h
@@ -7,27 +7,30 @@
 #include "snake.h"
 #include "fruits.h"
 #include "score.h"
+#include "sound.h"
 #include "highscore.h"
-
+#include <SDL_mixer.h>
 struct game
 {
     // Constructor
     game();
     //Destructor
     ~game();
-    void init(SDL_Renderer* renderer); //Khởi tạo các thành phần game : hình ảnh, font chữ
+    void init(SDL_Renderer* renderer); 
     void handleKeyPress(int key_code);
     void loop(bool& running);
     void render(SDL_Renderer* renderer);
 
-    background gameBackground;
-    image snakeImage; // quản lý hình ảnh rắn
-    image fruitImage; // quản lý hình ảnh fruit
 
-    snake player; // lớp rắn
-    fruits score; // lớp fruit
-    scoreGame point; // font hiển thị điểm
-    scoreGame bestPoint; // font hiển thị điểm cao
+
+    background gameBackground;
+    image snakeImage; 
+    image fruitImage; 
+
+    snake player; 
+    fruits score; 
+    scoreGame point; 
+    scoreGame bestPoint; 
     int countFruit = 0;
     int maxFruit = ReadFile();
     gameSound voice;
