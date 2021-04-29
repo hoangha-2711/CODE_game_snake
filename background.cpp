d1378d8223519b5c368a1bbcc88d8d0a3e018002
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2021-04-29 21:04:38 +0700

    Update background.cpp

--- background.cpp
+++ background.cpp
@@ -1,31 +1,52 @@
-#include "texture_manage.h"
-#include <SDL.h>
-#include <SDL_image.h>
+#include "background.h"
+#include <iostream>
+#include <string>
 
-using namespace std;
-
-SDL_Texture* loadTexture(SDL_Renderer* renderer, string path)
+background::background()
 {
-    //The final texture
-    SDL_Texture* newTexture = NULL;
+    background_game = NULL;
+    wall = NULL;
+}
+background::~background()
+{
+    background_game = NULL;
+    wall = NULL;
+}
+void background::init(SDL_Renderer* _renderer)
+{
+    renderer = _renderer;
+}
+void background::render(SDL_Renderer* des, SDL_Texture* texture, const int& rectX, const int& rectY, const int& reW, const int& reH, SDL_Rect* clip)
+{
+    SDL_Rect renderQuad = {rectX, rectY, reW, reH};
+    SDL_RenderCopy(des, texture , clip, &renderQuad);
+}
+void background::load()
+{
+    background_game = loadTexture(renderer, background_game, "background_black.png");
+    wall = loadTexture(renderer, wall,+ "block_square.png");
+}
+void background::display()
+{
+    render(renderer, background_game, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    //Load image at specified path
-    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-    if (loadedSurface == NULL)
+    // Load wall up and down
+    for(int x = 0; x <= SCREEN_WIDTH; x += WALL_CELL)
     {
-        cout << "Unable to load image " << path.c_str() << "! SDL_image Error: " << IMG_GetError() << "\n";
+        render(renderer, wall, x, 0, WALL_CELL, WALL_CELL);
+        render(renderer, wall, x, SCREEN_HEIGHT - WALL_CELL, WALL_CELL, WALL_CELL);
     }
-    else
+    // Load wall left and right
+    for(int y = 0; y <= SCREEN_HEIGHT; y += WALL_CELL)
     {
-        //Create texture from surface pixels
-        newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
-        if (newTexture == NULL)
-        {
-            cout << "Unable to create texture from " << path.c_str() << "! SDL Error: " << SDL_GetError() << "\n";
-        }
-
-        SDL_FreeSurface(loadedSurface);
+        render(renderer, wall, 0, y, WALL_CELL, WALL_CELL);
+        render(renderer, wall, SCREEN_WIDTH - WALL_CELL, y, WALL_CELL, WALL_CELL);
     }
-
-    return newTexture;
+}
+void background::free()
+{
+    SDL_DestroyTexture(background_game);
+    background_game = NULL;
+    SDL_DestroyTexture(wall);
+    wall = NULL;
 }
